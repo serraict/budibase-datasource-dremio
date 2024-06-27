c681a0d14d22f04cc7280eb5a9c4de615a8223a5
marijn <marijn@serraict.com>
2024-06-27 16:09:04 +0200

    Busy waiting for job to complete

--- src/datasource.ts
+++ src/datasource.ts
@@ -36,13 +36,47 @@ class CustomIntegration implements IntegrationBase {
   async create(query: { json: object }) {
     throw new Error("Only read operations are supported for now.");
   }
-
   async read(query: { queryString: string }) {
     var sql = this.api.SQL();
-    var data = await sql.query(query);
+    var job_data = await sql.query(query);
+    var job_id = job_data.id;
 
-    return { "query": query, "some": data };
+    var job_api = this.api.Job();
+
+    // Array to store all retrieved jobs
+    let jobsArray: any[] = [];
+
+    var polling_interval_ms = 250;
+    var timeout_ms = 30 * 1000;
+
+    // Wrap our polling logic in a new Promise
+    let intervalPromise = new Promise<void>((resolve, reject) => {
+
+      let intervalId = setInterval(async () => {
+        let job = await job_api.findById(job_id);
+        jobsArray.push(job);
+        if (job.jobState === "COMPLETED") {
+          clearInterval(intervalId);
+          resolve();
+        }
+      }, polling_interval_ms);
+
+      setTimeout(() => {
+        clearInterval(intervalId);
+        console.log('Stopped retrieving after 30 seconds.');
+        reject(new Error('Timed out'));
+      }, timeout_ms);
+    });
+
+    try {
+      await intervalPromise;
+    } catch (error) {
+      console.error(error);
+    }
+
+    return { "query": query, "data": jobsArray };
   }
+
 
   async update(query: { json: object }) {
     throw new Error("Only read operations are supported for now.");
